5a75b60ce3f8f5bd82ad249c58519cf2301ac65c
yann.cheyrezy <yann.cheyrezy@le-campus-numerique.fr>
2022-06-14 10:51:26 +0200

    modif layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -35,11 +35,8 @@
                     <li class="nav-item">
                         <a class="nav-link" href="cart">Panier</a>
                     </li>
+                    <li><img src=""</li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
             </div>
         </div>
     </nav>
