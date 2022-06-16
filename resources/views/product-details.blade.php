673ae0a220834d11f4244e514c2631f170786cb6
Mattéo GATEIL <matteo.gateil@Digital-Grenoble.local>
2022-06-16 16:59:13 +0200

    database import and first query

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -6,9 +6,13 @@
             <div class="texte">
                 <img src="{{$product['picture_url']}}" alt="product_image">
                 <p>{{$product['name']}}</p>
+                <p>{{$product['deqcription']}}</p>
                 <p>{{$product['price']}} €</p>
+                <p>{{$product['discount']}}</p>
                 <p>{{$product['weight']}} g</p>
-                <p>{{$product['discount']}} %</p>
+                <p>{{$product['url_image']}} %</p>
+                <p>{{$product['quantity']}}</p>
+                <p>{{$product['available']}}</p>
             </div>
     </div>
     @endsection
