dbb517f09532de2538b4d866151f112ace68eb9a
edouard.pelissot <edouard.pelissot@le-campus-numerique.fr>
2022-06-21 14:55:49 +0200

    test

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,7 +11,7 @@ class ProductController extends Controller
     {
         $products = \DB::select('SELECT * FROM products');
 //        dd($products);
-
+//kikou
         return view('product-list',['products' => $products]);
     }
 
